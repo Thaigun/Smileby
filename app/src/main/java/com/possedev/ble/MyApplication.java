993c772403fb8e95d2e7cd35bf73e3d837bf5766
Antti Mäkipää <antti.makipaa@aalto.fi>
2015-10-14 17:07:15 +0300

    Followed Estimote Android tutorial to make notifications on enter and exit of the beacons range.

---
+++ app/src/main/java/com/possedev/ble/MyApplication.java
@@ -0,0 +1,63 @@
+package com.possedev.ble;
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by Antti on 14.10.2015.
+ */
+public class MyApplication extends Application {
+
+    private BeaconManager beaconManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        beaconManager = new BeaconManager(getApplicationContext());
+
+        beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
+            @Override
+            public void onEnteredRegion(Region region, List<Beacon>list) {
+                showNotification("This is the title", "You entered the area");
+            }
+
+            @Override
+            public void onExitedRegion(Region region) {
+                showNotification("Exit title", "Exit message");
+            }
+        });
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startMonitoring(new Region("monitored region", UUID.fromString("b9407f30-f5f8-466e-aff9-25556b57fe6d"), 16432, 59483));
+            }
+        });
+    }
+
+    public void showNotification(String title, String message) {
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0, new Intent[] { notifyIntent}, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
+    }
+}
