169cb4dc62d9ecb5b667189bc052a5f34b136d84
Antti Mäkipää <antti.makipaa@aalto.fi>
2015-12-01 01:20:42 +0200

    Add MessagesAdapter

---
+++ Android/app/src/main/java/com/possedev/smileby/adapters/MessagesAdapter.java
@@ -0,0 +1,14 @@
+package com.possedev.smileby.adapters;
+
+import android.content.Context;
+
+/**
+ * Created by Antti on 30.11.2015.
+ */
+public class MessagesAdapter extends  {
+    private Context context;
+
+    public MessagesAdapter(Context c) {
+        context = c;
+    }
+}
